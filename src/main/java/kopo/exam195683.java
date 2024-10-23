db3e70b2649c4003dbf1403c396b6714bf7d90ce
donguk1 <donguk0105@naver.com>
2024-10-23 09:20:32 +0900

    운동 중독 플레이어

---
+++ src/main/java/kopo/exam195683.java
@@ -0,0 +1,29 @@
+package kopo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class exam195683 {
+
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] str = br.readLine().split(" ");
+
+        float w = Integer.parseInt(str[0]);
+        float r = Integer.parseInt(str[1]);
+
+        int rm = (int) (w * (1 + r / 30));
+
+        bw.write(rm + "");
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
