50b80684423d134b4164f3e4af69920b4f0af443
donguk1 <donguk0105@naver.com>
2024-10-23 09:18:19 +0900

    프로젝트 매니징

---
+++ src/main/java/kopo/exam195684.java
@@ -0,0 +1,38 @@
+package kopo;
+
+import java.io.*;
+
+public class exam195684 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        String[] hm = br.readLine().split(" ");
+        int h = Integer.parseInt(hm[0]);
+        int m = Integer.parseInt(hm[1]);
+
+        while(n-- != 0) {
+            int i = Integer.parseInt(br.readLine());
+            // h += i % 24;
+            m += i;
+
+        }
+
+        while(m > 59) {
+            h++;
+            m -= 60;
+        }
+
+        if (h > 23) {
+            h%=24;
+        }
+
+        bw.write(h + " " + m);
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
