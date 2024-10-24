37c3ab4b011f46ff04d554ce2f86c690c9fc5eaf
donguk1 <donguk0105@naver.com>
2024-10-24 11:22:31 +0900

    의좋은 형제

---
+++ src/main/java/kopo/exam49088.java
@@ -0,0 +1,43 @@
+package kopo;
+
+import java.io.*;
+import java.util.stream.Stream;
+
+public class exam49088 {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        int[] bro = Stream
+                .of(str.split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int d = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= d; i++) {
+            if (i % 2== 0) {
+                if (bro[1] % 2 != 0) {
+                    bro[0]++;
+                }
+
+                bro[1] /= 2;
+                bro[0] += bro[1];
+            } else {
+                if (bro[0] % 2 != 0) {
+                    bro[1]++;
+                }
+
+                bro[0] /= 2;
+                bro[1] += bro[0];
+            }
+        }
+
+        bw.write(bro[0] + " " + bro[1]);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
