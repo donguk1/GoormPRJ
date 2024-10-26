edab6d9d50a88038175646cab91ecd586e7b0479
donguk <donguk0105@naver.com>
2024-10-26 13:57:42 +0900

    별찍기(2)

---
+++ src/main/java/kopo/exam242374.java
@@ -0,0 +1,29 @@
+package kopo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class exam242374 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()) + 1;
+
+        while (n-- > 0) {
+            int i = n;
+            while (i-- > 0) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+
+    }
+}
