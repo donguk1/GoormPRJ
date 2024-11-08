26495d95d71307e20d973ea6ce1708ea79aa1102
donguk1 <donguk0105@naver.com>
2024-11-08 09:14:11 +0900

    동전 줍기 대회 3차 수정

--- src/main/java/kopo/level2/exam49078Fail.java
+++ src/main/java/kopo/level2/exam49078Fail.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class exam49078Fail {
     public static void main(String[] args) throws Exception {
@@ -14,52 +16,55 @@ public class exam49078Fail {
         int n = Integer.parseInt(br.readLine());
 
         int[] c = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
+                .mapToInt(num -> Math.abs(Integer.parseInt(num)))
+                .sorted()
                 .toArray();
 
         bw.write("c : " + Arrays.toString(c) + "\n");
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                if (Math.abs(c[i]) > Math.abs(c[j])) {
-                    int res = c[i];
-                    c[i] = c[j];
-                    c[j] = res;
-                }
-            }
-        }
+//        for (int i = 0; i < n; i++) {
+//            for (int j = i+1; j < n; j++) {
+//                if (Math.abs(c[i]) > Math.abs(c[j])) {
+//                    int res = c[i];
+//                    c[i] = c[j];
+//                    c[j] = res;
+//                }
+//            }
+//        }
+//        bw.write("c : " + Arrays.toString(c) + "\n");
 
-        bw.write("c : " + Arrays.toString(c) + "\n");
-
-        int minus = 0;
         int max = 0;
-        int res = Math.abs(c[0]);
+        int res = c[0];
+        List<Integer> list = new ArrayList<>(c[0]);
 
         for (int i = 0; i < c.length - 1; i++) {
 //            bw.write(c[i] + " ");
 
-            if (Math.abs(c[i + 1]) == Math.abs(c[i] + 1)) {
+            if (c[i + 1] == c[i] + 1) {
 
 //                bw.write("c[i] : " + c[i] + "\n");
 
-                res += Math.abs(c[i + 1]);
-                minus += c[i + 1];
+                res += c[i + 1];
+                list.add(i+1);
             } else {
 
+                if (max < res) {
+                    list.clear();
+                    list.add(i+1);
+
+                }
+
                 max = Math.max(max, res);
-                res = Math.abs(c[i + 1]);
-                minus = c[i + 1];
+                res = c[i + 1];
 
             }
+            bw.write(list.size() + "/" + list + "\n");
         }
 
-        if (minus < 0) {
-            bw.write("0");
-        }else {
-            max = Math.max(max, res);
+        max = Math.max(max, res);
 
-            bw.write(max + "");
-        }
+        bw.write(max + "\n");
+
         bw.flush();
         bw.close();
 
